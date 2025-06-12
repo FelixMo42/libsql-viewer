b5d6a7ed10cf507bd562fe46b6f99933eabf0009
Felix Moses <felix@tacocat.com>
2025-06-12 19:12:34 +0100

    Allow users to pass in a raw path to cli

--- dist/cli.js
+++ dist/cli.js
@@ -2,6 +2,7 @@
 import { createClient } from "@libsql/client";
 import { program } from "commander";
 import { libsqlViewer } from "./lib.js";
+import { pathToFileURL } from 'url';
 program
     .name("libsql-viewer")
     .description("A simple web viewer for libSQL databases")
@@ -27,13 +28,30 @@ function main() {
     };
     // Remove our extra options from the client config
     delete clientConfig.port;
+    // Make sure the user provided a URL
     if (!clientConfig.url) {
         console.error("Please provide a database URL as an argument or using --url.");
         process.exit(1);
     }
+    // Check if the user provided a path instead of a url
+    // If so, convert it to a file URL
+    if (!isUrl(clientConfig.url)) {
+        clientConfig.url = pathToFileURL(clientConfig.url).toString();
+        console.log(clientConfig.url);
+    }
+    // Run the viewer!
     libsqlViewer({
         client: createClient(clientConfig),
         port: program.opts().port
     });
+}
+function isUrl(str) {
+    try {
+        new URL(str);
+        return true;
+    }
+    catch {
+        return false;
+    }
 }
 main();
